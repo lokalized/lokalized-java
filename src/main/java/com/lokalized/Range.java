d6df4debbdb18fb46dfd74186dd7431c92436e25
Mark Allen <maa@xmog.com>
2017-05-04 07:38:15 -0400

    Ranges are now created via static methods; constructors are private

--- src/main/java/com/lokalized/Range.java
+++ src/main/java/com/lokalized/Range.java
@@ -20,6 +20,7 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.annotation.concurrent.Immutable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
@@ -35,7 +36,7 @@ import static java.util.Objects.requireNonNull;
  * Instead, you might supply a small representative range of values and specify the range is "infinite"
  * if it is understood that the range's pattern repeats indefinitely.
  * <p>
- * For example, you might have a powers-of-ten range with the 4 values {@code 1, 10, 100, 1000} and the "infinite" flag set.
+ * For example, you might generate an infinite powers-of-ten range with the 4 values {@code 1, 10, 100, 1000}.
  *
  * @author <a href="https://revetkn.com">Mark Allen</a>
  */
@@ -47,12 +48,62 @@ public class Range<T> {
   private final Boolean infinite;
 
   /**
+   * Provides an infinite range for the given values.
+   *
+   * @param values the values of the range, not null
+   * @param <T>    the type of values contained in the range
+   * @return an infinite range, not null
+   */
+  @Nonnull
+  public static <T> Range<T> ofInfiniteValues(@Nonnull Iterable<T> values) {
+    requireNonNull(values);
+    return new Range(values, true);
+  }
+
+  /**
+   * Provides an infinite range for the given values.
+   *
+   * @param values the values of the range, may be null
+   * @param <T>    the type of values contained in the range
+   * @return an infinite range, not null
+   */
+  @Nonnull
+  public static <T> Range<T> ofInfiniteValues(@Nullable T... values) {
+    return new Range(values, true);
+  }
+
+  /**
+   * Provides a finite range for the given values.
+   *
+   * @param values the values of the range, not null
+   * @param <T>    the type of values contained in the range
+   * @return a finite range, not null
+   */
+  @Nonnull
+  public static <T> Range<T> ofFiniteValues(@Nonnull Iterable<T> values) {
+    requireNonNull(values);
+    return new Range(values, false);
+  }
+
+  /**
+   * Provides a finite range for the given values.
+   *
+   * @param values the values of the range, may be null
+   * @param <T>    the type of values contained in the range
+   * @return a finite range, not null
+   */
+  @Nonnull
+  public static <T> Range<T> ofFiniteValues(@Nullable T... values) {
+    return new Range(values, false);
+  }
+
+  /**
    * Creates a range with the given values and "infinite" flag.
    *
    * @param values   the values that comprise this range, not null
    * @param infinite whether this range is infinite - that is, whether the range's pattern repeats indefinitely, not null
    */
-  public Range(@Nonnull Iterable<T> values, @Nonnull Boolean infinite) {
+  private Range(@Nonnull Iterable<T> values, @Nonnull Boolean infinite) {
     requireNonNull(values);
     requireNonNull(infinite);
 
@@ -60,6 +111,20 @@ public class Range<T> {
     values.forEach(valuesAsList::add);
 
     this.values = Collections.unmodifiableList(valuesAsList);
+    this.infinite = infinite;
+  }
+
+  /**
+   * Creates a range with the given values and "infinite" flag.
+   *
+   * @param values   the values that comprise this range, may be null
+   * @param infinite whether this range is infinite - that is, whether the range's pattern repeats indefinitely, not null
+   */
+  private Range(@Nullable T[] values, @Nonnull Boolean infinite) {
+    requireNonNull(values);
+    requireNonNull(infinite);
+
+    this.values = values == null ? Collections.emptyList() : Collections.unmodifiableList(Arrays.asList(values));
     this.infinite = infinite;
   }
 
