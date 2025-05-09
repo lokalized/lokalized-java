3acd1bd4d8bcf53f519a5911f1b6d9840473825f
Mark Allen <mark.a.allen@gmail.com>
2025-05-09 11:44:14 -0400

    Update copyright information to reflect 2025

--- src/main/java/com/lokalized/LocalizedStringUtils.java
+++ src/main/java/com/lokalized/LocalizedStringUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Transmogrify LLC.
+ * Copyright 2017-2022 Product Mog LLC, 2022-2025 Revetware LLC.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
