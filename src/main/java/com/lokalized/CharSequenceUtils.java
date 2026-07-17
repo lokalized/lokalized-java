13124ebabf199dc436a102cf675061d97f6a5521
Mark Allen <mark.a.allen@gmail.com>
2026-07-17 12:21:02 -0400

    Missed in last commit

---
+++ src/main/java/com/lokalized/CharSequenceUtils.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2017-2022 Product Mog LLC, 2022-2026 Revetware LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lokalized;
+
+import org.jspecify.annotations.NonNull;
+
+import javax.annotation.concurrent.ThreadSafe;
+
+import static java.lang.String.format;
+import static java.util.Objects.requireNonNull;
+
+/** Internal bounded conversions for caller-supplied character sequences. */
+@ThreadSafe
+final class CharSequenceUtils {
+  private CharSequenceUtils() {
+    // Non-instantiable
+  }
+
+  @NonNull
+  static String toString(@NonNull CharSequence value, int maximumCharacters, @NonNull String description) {
+    requireNonNull(value);
+    requireNonNull(description);
+
+    if (maximumCharacters <= 0)
+      throw new IllegalArgumentException("maximumCharacters must be positive");
+
+    int length = value.length();
+
+    if (length > maximumCharacters)
+      throw new IllegalArgumentException(format(
+          "%s exceeds the maximum of %d characters", description, maximumCharacters));
+
+    if (value instanceof String)
+      return (String) value;
+
+    StringBuilder result = new StringBuilder(length);
+    result.append(value, 0, length);
+    return result.toString();
+  }
+}
